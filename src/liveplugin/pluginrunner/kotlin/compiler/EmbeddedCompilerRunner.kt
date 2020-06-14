fcff65236d823596acd739990490fa7d800d1ce7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-06-14 14:20:39 +0100

    make it work with kotlin 1.3.72 api

--- src/liveplugin/pluginrunner/kotlin/compiler/EmbeddedCompilerRunner.kt
+++ src/liveplugin/pluginrunner/kotlin/compiler/EmbeddedCompilerRunner.kt
@@ -13,24 +13,27 @@ import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles.JVM_CONFIG_F
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinToJVMBytecodeCompiler
 import org.jetbrains.kotlin.cli.jvm.config.JvmClasspathRoot
+import org.jetbrains.kotlin.cli.jvm.plugins.PluginCliParser
 import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.config.CommonConfigurationKeys.MODULE_NAME
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.JVMConfigurationKeys.*
-import org.jetbrains.kotlin.script.KotlinScriptDefinition
+import org.jetbrains.kotlin.config.JVMConfigurationKeys.OUTPUT_DIRECTORY
+import org.jetbrains.kotlin.config.JVMConfigurationKeys.RETAIN_OUTPUT_IN_MEMORY
+import org.jetbrains.kotlin.scripting.configuration.ScriptingConfigurationKeys.SCRIPT_DEFINITIONS
+import org.jetbrains.kotlin.scripting.definitions.ScriptDefinition
 import java.io.File
-import kotlin.jvm.internal.Reflection
+import kotlin.reflect.KClass
+import kotlin.script.experimental.host.ScriptingHostConfiguration
+import kotlin.script.experimental.host.getScriptingClass
+import kotlin.script.experimental.jvm.JvmGetScriptingClass
 
-
-@Suppress("unused")
 fun compile(
     sourceRoot: String,
     classpath: List<File>,
     outputDirectory: File,
-    kotlinScriptTemplateClass: Class<*>
+    kotlinScriptTemplateClass: KClass<*>
 ): List<String> {
     val rootDisposable = Disposer.newDisposable()
-
     try {
         val messageCollector = ErrorMessageCollector()
         val configuration = createCompilerConfiguration(sourceRoot, classpath, outputDirectory, messageCollector, kotlinScriptTemplateClass)
@@ -47,13 +50,11 @@ fun compile(
     }
 }
 
-private class ErrorMessageCollector: MessageCollector {
+private class ErrorMessageCollector : MessageCollector {
     val errors = ArrayList<String>()
 
     override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageLocation?) {
-        if (severity == ERROR || severity == EXCEPTION) {
-            errors.add(PLAIN_FULL_PATHS.render(severity, message, location))
-        }
+        if (severity == ERROR || severity == EXCEPTION) errors.add(PLAIN_FULL_PATHS.render(severity, message, location))
     }
 
     override fun clear() = errors.clear()
@@ -67,21 +68,29 @@ private fun createCompilerConfiguration(
     classpath: List<File>,
     outputDirectory: File,
     messageCollector: MessageCollector,
-    kotlinScriptTemplateClass: Class<*>
+    kotlinScriptTemplateClass: KClass<*>
 ): CompilerConfiguration {
     return CompilerConfiguration().apply {
         put(MODULE_NAME, "KotlinCompilerWrapperModule")
         put(MESSAGE_COLLECTOR_KEY, messageCollector)
-        add(SCRIPT_DEFINITIONS, KotlinScriptDefinition(Reflection.createKotlinClass(kotlinScriptTemplateClass)))
+        add(SCRIPT_DEFINITIONS, ScriptDefinition.FromTemplate(
+            ScriptingHostConfiguration { getScriptingClass(JvmGetScriptingClass()) },
+            kotlinScriptTemplateClass
+        ))
 
         add(CONTENT_ROOTS, KotlinSourceRoot(path = sourceRoot, isCommon = false))
-
         classpath.forEach { path ->
             add(CONTENT_ROOTS, JvmClasspathRoot(path))
         }
+
+        // Based on org.jetbrains.kotlin.script.ScriptTestUtilKt#loadScriptingPlugin
+        PluginCliParser.loadPluginsSafe(
+            pluginOptions = null,
+            configuration = this,
+            pluginClasspaths = classpath.map { it.path }
+        )
 
         put(RETAIN_OUTPUT_IN_MEMORY, false)
         put(OUTPUT_DIRECTORY, outputDirectory)
     }
 }
-
--- test/liveplugin/pluginrunner/kotlin/compiler/EmbeddedCompilerRunnerTests.kt
+++ test/liveplugin/pluginrunner/kotlin/compiler/EmbeddedCompilerRunnerTests.kt
@@ -6,49 +6,61 @@ import org.junit.Assert.assertTrue
 import org.junit.Ignore
 import org.junit.Test
 import java.io.File
+import kotlin.script.experimental.annotations.KotlinScript
 
-@Ignore // Run manually because gradle won't have "kotlin_stdlib_path".
+@Ignore // Run manually because KtsScriptFixture needs paths to stdlib and kotlin scripting jars
 class EmbeddedCompilerRunnerTests {
-    @Test fun `can compile an empty file`() =
-        KtsScriptFixture(
-            scriptSourceCode = ""
-        ).run {
-            assertThat(compile(), equalTo(emptyList()))
-        }
+    @Test fun `can compile an empty file`() = KtsScriptFixture(
+        scriptSourceCode = ""
+    ).run {
+        assertThat(compileScript(), equalTo(emptyList()))
+    }
 
-    @Test fun `can compile println`() =
-        KtsScriptFixture(
-            scriptSourceCode = "println(123)"
-        ).run {
-            assertThat(compile(), equalTo(emptyList()))
-        }
+    @Test fun `can compile println`() = KtsScriptFixture(
+        scriptSourceCode = "println(123)"
+    ).run {
+        assertThat(compileScript(), equalTo(emptyList()))
+    }
 
-    @Test fun `fails to compile unresolved reference`() =
-        KtsScriptFixture(
-            scriptSourceCode = "nonExistingFunction()"
-        ).run {
-            val errors = compile()
-            assertThat(errors.size, equalTo(1))
-            assertTrue(errors.first().contains("unresolved reference: nonExistingFunction"))
-        }
+    @Test fun `fails to compile unresolved reference`() = KtsScriptFixture(
+        scriptSourceCode = "nonExistingFunction()"
+    ).run {
+        val errors = compileScript()
+        assertThat(errors.size, equalTo(1))
+        assertTrue(errors.first().contains("unresolved reference: nonExistingFunction"))
+    }
 }
 
-data class KtsScriptFixture(
+private data class KtsScriptFixture(
     val srcDir: File = createTempDir(),
     val outputDir: File = createTempDir(),
-    val kotlinStdLibPath: String = System.getenv("kotlin_stdlib_path"),
+    val kotlinStdLibPath: String = System.getenv("kotlin-stdlib-path"),
+    val kotlinScriptRuntimePath: String = System.getenv("kotlin-script-runtime-path"),
+    val kotlinScriptCommonPath: String = System.getenv("kotlin-script-common-path"),
+    val kotlinScriptJvmPath: String = System.getenv("kotlin-script-jvm"),
+    val kotlinScriptCompilerEmbeddablePath: String = System.getenv("kotlin-script-compiler-embeddable-path"),
+    val kotlinScriptCompilerImplEmbeddablePath: String = System.getenv("kotlin-script-compiler-impl-embeddable-path"),
     val scriptSourceCode: String
 ) {
     init {
         createTempFile(suffix = ".kts", directory = srcDir).writeText(scriptSourceCode)
     }
 
-    fun compile(): List<String> = compile(
+    fun compileScript(): List<String> = compile(
         sourceRoot = srcDir.absolutePath,
-        classpath = listOf(File(kotlinStdLibPath), File("build/classes/kotlin/test/")),
+        classpath = listOf(
+            File(kotlinStdLibPath),
+            File(kotlinScriptJvmPath),
+            File(kotlinScriptCommonPath),
+            File(kotlinScriptRuntimePath),
+            File(kotlinScriptCompilerEmbeddablePath),
+            File(kotlinScriptCompilerImplEmbeddablePath),
+            File("build/classes/kotlin/test/")
+        ),
         outputDirectory = outputDir,
-        kotlinScriptTemplateClass = EmptyKotlinScriptTemplate::class.java
+        kotlinScriptTemplateClass = EmptyKotlinScriptTemplate::class
     )
-
-    class EmptyKotlinScriptTemplate
 }
+
+@KotlinScript
+class EmptyKotlinScriptTemplate
